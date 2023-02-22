1f9fdfda5d3077157088540d548b027410ce784e
Janonez <906269785@qq.com>
2023-02-22 18:11:10 +0800

    进程状态

--- lesson11/test.c
+++ lesson11/test.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
+
 int main()
 {
-  while(1)
+  
+
+  //int count = 0;
+  //while(1)
+  //{
+    //int a = 0;
+    //scanf("%d",&a);
+    //printf("%d\n",a);
+    //printf("我在运行吗？？%d\n",count++);
+    //sleep(1);
+  //}
+  //int result  = 30;
+  //if(result == 10)  return 0;
+  //else return 3;
+  //
+  pid_t id = fork();
+  if(id == 0)
   {
-    printf("我在运行吗？？？\n");
+    // 子进程
+    while(1)
+    {
+      printf("我是子进程，我在运行，pid：%d，ppid：%d\n",getpid(),getppid());
+      sleep(1);
+    }
+  }
+  else if(id > 0)
+  {
+    // 父进程
+    while(1)
+    {
+      printf("我是父进程，我在运行，pid：%d，ppid：%d\n",getpid(),getppid());
+      sleep(1);
+    }
   }
   return 0;
 }
