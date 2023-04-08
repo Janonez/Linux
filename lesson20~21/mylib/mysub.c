1b59a7edb0c55ccf0007254e3bb35f16f30d9d62
Janonez <906269785@qq.com>
2023-04-09 00:02:01 +0800

    动静态库和管道、

---
+++ lesson20~21/mylib/mysub.c
@@ -0,0 +1,7 @@
+#include "mysub.h"
+
+
+int mysub(int x, int y)
+{
+  return x - y;
+}
---
+++ lesson20~21/others/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "myadd.h"
+#include "mysub.h"
+
+int main()
+{
+  int x = 100;
+  int y = 30;
+  printf("%d + %d = %d\n",x, y, myadd(x,y));
+  printf("%d - %d = %d\n",x, y, mysub(x,y));
+  return 0;
+}
